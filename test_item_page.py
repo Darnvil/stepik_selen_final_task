ef6bc5aee26e543e74621fa3b7a86fd286a8ee15
Darnvil <twitchpgpr@mail.ru>
2023-02-14 13:02:15 +0300

    code prepared for final review

--- test_item_page.py
+++ test_item_page.py
@@ -8,9 +8,7 @@ from .pages.basket_page import BasketPage
 
 link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 
-
-@pytest.mark.usertest
-class TestUserAddToBasketFromProductPage():
+class TestUserAddToBasketFromItemPage():
     @pytest.fixture(scope='function', autouse=True)
     def setup(self, browser):
         link = 'http://selenium1py.pythonanywhere.com/accounts/login/'
@@ -19,6 +17,7 @@ class TestUserAddToBasketFromProductPage():
         page.register_new_user(str(time.time()) + '@fakegmail.com', str(time.time()) + '5678')
         page.should_be_authorized_user()
 
+    @pytest.mark.need_review
     def test_user_can_add_item_to_basket(self, browser):
         link = f'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
         page = ItemPage(browser, link)
@@ -34,47 +33,54 @@ class TestUserAddToBasketFromProductPage():
         page.open()
         page.should_not_be_success_message()
 
-@pytest.mark.xfail
-def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
-    link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
-    page = ItemPage(browser, link)
-    page.open()
-    page.add_to_basket()
-    page.should_not_be_success_message()
 
+class TestGuestItemPageAddToBasket():
+    @pytest.mark.need_review
+    def test_guest_can_add_item_to_basket(self, browser):
+        link = f'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+        page = ItemPage(browser, link)
+        page.open()
+        page.add_to_basket()
+        item_name = page.get_item_name()
+        page.should_be_success_message(item_name)
+        page.should_be_basket_sum_info()
 
+    @pytest.mark.need_review
+    def test_guest_can_go_to_login_page_from_item_page(self, browser):
+        link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+        page = ItemPage(browser, link)
+        page.open()
+        page.go_to_login_page()
+        login_page = LoginPage(browser, browser.current_url)
+        login_page.should_be_login_page()
 
+    @pytest.mark.need_review
+    def test_guest_cant_see_product_in_basket_opened_from_item_page(self, browser):
+        page = ItemPage(browser, link)
+        page.open()
+        page.go_to_basket()
+        basket_page = BasketPage(browser, browser.current_url)
+        basket_page.should_be_empty_basket()
 
+    @pytest.mark.xfail
+    def test_guest_cant_see_success_message_after_adding_product_to_basket(self, browser):
+        link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+        page = ItemPage(browser, link)
+        page.open()
+        page.add_to_basket()
+        page.should_not_be_success_message()
 
-@pytest.mark.xfail
-def test_message_disappeared_after_adding_product_to_basket(browser):
-    link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
-    page = ItemPage(browser, link)
-    page.open()
-    page.add_to_basket()
-    page.should_be_disappeared_message()
+    @pytest.mark.xfail
+    def test_message_disappeared_after_adding_product_to_basket(self, browser):
+        link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+        page = ItemPage(browser, link)
+        page.open()
+        page.add_to_basket()
+        page.should_be_disappeared_message()
 
-
-def test_guest_should_see_login_link_on_item_page(browser):
-    link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
-    page = ItemPage(browser, link)
-    page.open()
-    page.add_to_basket()
-    page.should_be_login_link()
-
-
-def test_guest_can_go_to_login_page(browser):
-    link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
-    page = ItemPage(browser, link)
-    page.open()
-    page.go_to_login_page()
-    login_page = LoginPage(browser, browser.current_url)
-    login_page.should_be_login_page()
-
-
-def test_guest_cant_see_product_in_basket_opened_from_item_page(browser):
-    page = ItemPage(browser, link)
-    page.open()
-    page.go_to_basket()
-    basket_page = BasketPage(browser, browser.current_url)
-    basket_page.should_be_empty_basket()
+    def test_guest_should_see_login_link_on_item_page(self, browser):
+        link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+        page = ItemPage(browser, link)
+        page.open()
+        page.add_to_basket()
+        page.should_be_login_link()
